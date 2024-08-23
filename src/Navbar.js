79e75b7478c8143677b52389089daf9670927c73
Mohammed Sinhah <sinhah@Mohammeds-Air.home>
2024-08-23 00:19:05 -0400

    second commit

---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return (  
+        <nav className="navbar">
+            <h1>Bank United</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/">View My Account</a>
+                <a href="./pages/Login" style={{
+                    color: 'white',
+                    backgroundColor: '#f1356d',
+                    borderRadius: '8px'
+                }}>Sign In</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
